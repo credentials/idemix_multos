eba075f8d9a10b3c62d1cd05371559719ecd4629
Pim Vullers <pim@vullersmail.nl>
2012-10-17 14:31:06 +0200

    Fixed attribute selection, added expiry disclosure enforcement (requires EXPIRY define) which fixes #20

--- src/crypto_proving.c
+++ src/crypto_proving.c
@@ -37,23 +37,42 @@
 /********************************************************************/
 
 /**
- * Select the attributes to be disclosed
+ * Select the attributes to be disclosed.
+ *
+ * @param selection bitmask of attributes to be disclosed.
  */
 void selectAttributes(int selection) {
-  int i;
 
+  // Do not allow modification of the selection within a protocol session.
   if (session.prove.disclose != 0) {
-      debugError("selectAttributes(): selection already made");
-      ReturnSW(ISO7816_SW_COMMAND_NOT_ALLOWED_AGAIN);
+    debugError("selectAttributes(): selection cannot be modified once set");
+    ReturnSW(ISO7816_SW_COMMAND_NOT_ALLOWED_AGAIN);
   }
 
-  for (i = 0; i < sizeof(int); i++) {
-    if (((1 << i) & selection) != 0 && (i == 0 || i > credential->size)) {
-      debugError("selectAttributes(): invalid attribute index");
-      credential = NULL;
-      ReturnSW(ISO7816_SW_WRONG_DATA);
-    }
+  // Never disclose the master secret.
+  if (selection & 0x0001 != 0) {
+    debugError("selectAttributes(): master secret cannot be disclosed");
+    credential = NULL;
+    ReturnSW(ISO7816_SW_WRONG_DATA);
   }
+
+#ifdef EXPIRY
+  // Always disclose the expiry attribute.
+  if (selection & 0x0002 == 0) {
+    debugError("selectAttributes(): expiry attribute must be disclosed");
+    credential = NULL;
+    ReturnSW(ISO7816_SW_WRONG_DATA);
+  }
+#endif // EXPIRY
+
+  // Do not allow non-existant attributes.
+  if (selection & (0xFFFF << credential->size + 1) != 0) {
+    debugError("selectAttributes(): selection contains non-existant attributes");
+    credential = NULL;
+    ReturnSW(ISO7816_SW_REFERENCED_DATA_NOT_FOUND);
+  }
+
+  // Set the attribute disclosure selection.
   session.prove.disclose = selection;
   debugInteger("Disclosure selection", session.prove.disclose);
 }
